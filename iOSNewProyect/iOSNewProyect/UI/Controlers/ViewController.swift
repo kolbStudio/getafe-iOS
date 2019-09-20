4f3d218ef40d41de25da9ae3a8d2fcc3280eb9e4
Joseph Robert <colombojr38@gmail.com>
2019-09-20 17:52:55 +0200

    project

--- iOSNewProyect/iOSNewProyect/UI/Controlers/ViewController.swift
+++ iOSNewProyect/iOSNewProyect/UI/Controlers/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    }[<#indexSet#> enumerateIndexesWithOptions:NSEnumerationReverse usingBlock:^(NSUInteger index, BOOL *stop) {
+    <#statements#>
+    }];
 
 
 }
