aad2cb2e8b869de38aab15ebeb89749ca097a8c9
Joseph Robert <colombojr38@gmail.com>
2019-09-20 19:27:52 +0200

    Create Student model

---
+++ iOSNewProyect/iOSNewProyect/Data/Local/Models/Student.swift
@@ -0,0 +1,30 @@
+//
+//  Student.swift
+//  iOSNewProyect
+//
+//  Created by Dev2 on 20/09/2019.
+//  Copyright © 2019 kolbStudio. All rights reserved.
+//
+
+import Foundation
+
+class Student: CustomStringConvertible {
+   
+    var name: String?
+    var email: String?
+    var birthdate: Date?
+    
+    var description: String {
+        return "Student data: \(String(describing: name)) \(String(describing: email))"
+    }
+    
+    
+    convenience init(name: String, email: String? = nil, birthdate: Date? = Date()) {
+        self.init()
+        
+        self.name = name
+        self.email = email
+        self.birthdate = birthdate
+    }
+}
+
--- iOSNewProyect/iOSNewProyect/UI/Controlers/ViewController.swift
+++ iOSNewProyect/iOSNewProyect/UI/Controlers/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }[<#indexSet#> enumerateIndexesWithOptions:NSEnumerationReverse usingBlock:^(NSUInteger index, BOOL *stop) {
-    <#statements#>
-    }];
+    }
 
 
 }
